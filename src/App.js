211820c6e10cbf7ba96fed81f8583f7568d6caa3
cruzrafaelx <rafaelcruz@outlook.de>
2025-02-11 06:48:23 +0100

    Bootstrap Home and About components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <Link className='logo' to="/">#VANLIFE</Link>
+        <div className='links-right'>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </div>
       </header>
-    </div>
+      
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+        </Routes>
+      </main>
+    
+      <footer>
+        <p>Ⓒ 2022 #VANLIFE</p>
+      </footer>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/Components/About.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function About(){
+    return(
+      <div className="about-container">
+        <img className="about-img" src="http://localhost:3000/bg-images/image55.png"></img>
+
+        <div className="about-text">
+          <h1>Don’t squeeze in a sedan when you could relax in a van</h1>
+          <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.(Hitch costs extra 😉)</p>
+          <br/>
+          <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+        </div>
+        
+
+        <div className="about-cta">
+          <h1>Your destination is waiting. <br/>Your van is ready.</h1>
+          <button>Explore our vans</button>
+        </div>
+      </div>
+    )
+  }
